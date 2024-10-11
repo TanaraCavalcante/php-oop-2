c3d7ee40fac3ecaf934700ff26d7a8e839c64367
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-11 15:59:27 -0400

    Category.php

--- index.php
+++ index.php
@@ -3,3 +3,7 @@ L'e-commerce vende prodotti per animali.
 I prodotti sono categorizzati, le categorie sono Cani o Gatti.
 I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
 Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+
+<?php
+ include __DIR__ .('/plp-classes/category.php');
+?>
---
+++ php-classes/category.php
@@ -0,0 +1,18 @@
+<!-- I prodotti sono categorizzati, le categorie sono Cani o cat. -->
+
+<?php
+//! Creo le variabili della stanza
+class Category {
+    public $dog;
+    public $cat;
+
+    //! Cronstruct
+    function __Contruct(string $dog, string $cat){
+        $this->dog = $dog
+        $this->cat = $cat
+    }
+
+  
+
+    
+}
